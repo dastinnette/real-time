af44fe9fc17a070d8f8bbbf90f064af129373a8c
David Stinnette <dastinnette@gmail.com>
2016-04-23 13:11:03 -0600

    Communicating connection count between client and server

--- server.js
+++ server.js
@@ -19,4 +19,12 @@ const server = http.createServer(app)
 const socketIo = require('socket.io');
 const io = socketIo(server);
 
+io.on('connection', function (socket) {
+  console.log('A user has connected.', io.engine.clientsCount);
+
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+  });
+});
+
 module.exports = server;
