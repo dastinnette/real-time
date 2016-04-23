f3a0fe0457343179bede89c7388bf914370c4f88
David Stinnette <dastinnette@gmail.com>
2016-04-23 16:00:13 -0600

    Adds procfile

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const http = require('http');
 const express = require('express');
-
 const app = express();
+const port = process.env.PORT || 3000;
+const server = http.createServer(app)
+                   .listen(port, function () {
+                     console.log('Listening on port ' + port + '.');
+                   });
+const socketIo = require('socket.io');
+const io = socketIo(server);
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
-
-const port = process.env.PORT || 3000;
-
-const server = http.createServer(app)
-                 .listen(port, function () {
-                    console.log('Listening on port ' + port + '.');
-                  });
-
-const socketIo = require('socket.io');
-const io = socketIo(server);
 
 var votes = {};
 
