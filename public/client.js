58a901b7ebb57175d88381ccfef86630a7e960ea
David Stinnette <dastinnette@gmail.com>
2016-04-24 17:39:26 -0600

    Create poll view

--- public/client.js
+++ public/client.js
@@ -1,18 +1,17 @@
 var socket = io();
 
 var connectionCount = document.getElementById('connection-count');
-var buttons = document.querySelectorAll('#choices button');
 
 socket.on('usersConnected', function (count) {
   connectionCount.innerText = 'Connected Users: ' + count;
 });
 
+var buttons = document.querySelectorAll('#poll button');
+var pollID = document.location.href.split('/')[4];
+console.log(pollID);
+
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function () {
-    socket.send('voteCast', this.innerText);
+    socket.send('voteCast', {vote: this.innerText, id: pollID});
   });
 }
-
-socket.on('voteCount', function (votes) {
-  console.log(votes);
-});
--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const http = require('http');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
-const server = http.createServer(app)
-                   .listen(port, function () {
-                     console.log('Listening on port ' + port + '.');
-                   });
-const socketIo = require('socket.io');
-const io = socketIo(server);
+const path = require('path');
 const bodyParser = require('body-parser');
 const generateId = require('./lib/generate-id');
+const port = process.env.PORT || 3000;
+const server = http.createServer(app)
+                 .listen(port, function () {
+                    console.log('Listening on port ' + port + '.');
+                  });
+const socketIo = require('socket.io');
+const io = socketIo(server);
 
-app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 app.set('view engine', 'ejs');
 
 app.locals.polls = {};
-app.locals.url = "https://localhost:3000/";
+app.locals.title = "Crowdsource";
 
 app.get('/', function (req, res){
   res.sendFile(__dirname + '/public/index.html');
@@ -27,16 +29,27 @@ app.post('/polls', (request, response) => {
   if (!request.body.poll) { return response.sendStatus(400); }
   var id = generateId();
   var admin = generateId();
-  app.locals.polls[id] = {};
-  app.locals.polls[id].question = request.body.poll.question;
-  app.locals.polls[id].options = request.body.poll.options;
+  app.locals.polls[id] = request.body.poll;
   app.locals.polls[id].adminID = admin;
-  console.log(app.locals.polls[id]);
+  app.locals.polls[id].url = "http://localhost:3000/polls/" + id;
+  app.locals.polls[id].votes = {};
+  app.locals.polls[id].closed = false;
   response.redirect('/polls/' + id + '/admin/' + admin );
 });
 
+app.get('/polls/:pollID', function (req, res){
+  var pollID = req.params.pollID;
+  var poll = app.locals.polls[pollID];
+  res.render('poll', {pollID: pollID,
+                      question: poll.question,
+                      options: poll.options});
+});
+
 app.get('/polls/:pollID/admin/:adminID', function (req, res){
-  res.render('admin-poll', {pollID : req.params.pollID});
+  var pollID = req.params.pollID;
+  var poll = app.locals.polls[pollID];
+  res.render('admin', {pollID: pollID,
+                            url: poll.url});
 });
 
 io.on('connection', function (socket) {
@@ -44,22 +57,23 @@ io.on('connection', function (socket) {
 
   io.sockets.emit('usersConnected', io.engine.clientsCount);
 
+  socket.on('message', function (channel, message) {
+    if (channel === 'voteCast') {
+      app.locals.polls[message.id].votes[socket.id] = message.vote;
+    }
+  });
+
   socket.on('disconnect', function () {
     console.log('A user has disconnected.', io.engine.clientsCount);
-    io.sockets.emit('userConnection', io.engine.clientsCount);
+    io.sockets.emit('usersConnected', io.engine.clientsCount);
   });
+
 });
 
-function countVotes(votes) {
-var voteCount = {
-    "This is old hat to me": 0,
-    "I have an okay understanding of this": 0,
-    "I have no idea what you're babbling about": 0
-};
-  for (var vote in votes) {
-    voteCount[votes[vote]]++
-  }
-  return voteCount;
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
 }
 
 module.exports = server;
