16199b124a2100309b910e55e89667a6b8024b21
David Stinnette <dastinnette@gmail.com>
2016-04-24 23:37:03 -0600

    Added fixture to successfully test POST

---
+++ test/fixtures/new-poll.js
@@ -0,0 +1,8 @@
+exports.validPoll = {
+  question: 'How geeked are you now that the Cubs are finally good?',
+  options: [ 'I will never know joy like this again',
+             'I will sell my house for playoff tickets',
+             'Meh',
+             'Baseball? Hard pass' ],
+  share: 'on'
+};
--- test/server-test.js
+++ test/server-test.js
@@ -1,6 +1,7 @@
-const assert = require('assert');
+const assert = require('chai').assert;
 const request = require('request');
 const app = require('../server');
+const newPoll = require('./fixtures/new-poll');
 
 describe('Server', () => {
 
@@ -33,5 +34,53 @@ describe('Server', () => {
       });
     });
 
+    it('should have a form', (done) => {
+      var title = app.locals.title;
+
+      this.request.get('/', (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes(title),
+               `"${response.body}" does not include "${title}".`);
+        done();
+      });
+    });
   });
+
+  describe('POST /polls', () => {
+    beforeEach(() => {
+      app.locals.polls = {};
+    });
+
+    it('should not return 404', (done) => {
+      this.request.post('/polls', (error, response) => {
+        if (error) { done(error); }
+        assert.notEqual(response.statusCode, 404);
+        done();
+      });
+    });
+
+    it('should receive and store data', (done) => {
+      var poll = { poll: newPoll.validPoll };
+
+      this.request.post('/polls', { form: poll }, (error, response) => {
+        if (error) { done(error); }
+        var pollCount = Object.keys(app.locals.polls).length;
+        assert.equal(pollCount, 1, `Expected 1 poll, found ${pollCount}`);
+        done();
+      });
+    });
+
+    it('should redirect the user to their new poll', (done) => {
+      var poll = { poll: newPoll.validPoll };
+
+      this.request.post('/polls', { form: poll }, (error, response) => {
+        if (error) { done(error); }
+        var newPollId = Object.keys(app.locals.polls)[0];
+        var newAdminId = app.locals.polls[newPollId].adminID;
+        assert.equal(response.headers.location, '/polls/' + newPollId + '/admin/' + newAdminId);
+        done();
+      });
+    });
+  });
+
 });
