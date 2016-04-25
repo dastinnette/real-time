eaf1f385ca31feb3d17f33382ea2b32862e532e2
David Stinnette <dastinnette@gmail.com>
2016-04-25 08:02:07 -0600

    Refactor views for better UI

--- public/client.js
+++ public/client.js
@@ -1,13 +1,12 @@
 var socket = io();
-
 var connectionCount = document.getElementById('connection-count');
+var buttons = document.querySelectorAll('#poll button');
+var pollID = document.location.href.split('/')[4];
+var pollThings = document.getElementById('#poll-things');
 
 socket.on('usersConnected', function (count) {
   connectionCount.innerText = 'Connected Users: ' + count;
 });
-
-var buttons = document.querySelectorAll('#poll button');
-var pollID = document.location.href.split('/')[4];
 
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function () {
@@ -40,15 +39,13 @@ if ($('#close-poll')[0]) {
   });
 }
 
-var pollThings = document.getElementById('#poll-things');
-
 socket.on('pollClosed', function (message) {
   $pollThings = $('#poll-things');
   if ($pollThings !== null && message.pollID === pollID) {
-    $pollThings.empty().append('<p>This poll is closed</p>');
+    $pollThings.empty().append('<h3>This poll is closed</h3>');
   }
   $pollStatus = $('#poll-status');
   if ($pollStatus !== null && message.pollID === pollID) {
-    $pollStatus.empty().append('<p>Poll Status: Closed</p>');
+    $pollStatus.empty().append('<h3>Closed</h3>');
   }
 });
