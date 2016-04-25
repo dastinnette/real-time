1e63c470a5f11c0a0917e06a0b8cd131aa45adf3
David Stinnette <dastinnette@gmail.com>
2016-04-24 18:17:26 -0600

    Counting votes on client side

--- public/client.js
+++ public/client.js
@@ -8,10 +8,27 @@ socket.on('usersConnected', function (count) {
 
 var buttons = document.querySelectorAll('#poll button');
 var pollID = document.location.href.split('/')[4];
-console.log(pollID);
 
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function () {
     socket.send('voteCast', {vote: this.innerText, id: pollID});
   });
 }
+
+socket.on('voteCount', function (votes) {
+  console.log(votes);
+  $results = $('#results');
+  if ($results !== null && votes.pollID === pollID) {
+    $('#results').empty();
+    for (var option in votes.votes){
+      $('#results').append('<p>' + option + ': ' + votes.votes[option] + '<p>');
+    }
+  }
+});
+
+socket.on('voteRecorded', function (message) {
+  $selection = $('#selection');
+  if ($selection !== null) {
+    $('#selection').empty().append('<p>' + message + '<p>');
+  }
+});
